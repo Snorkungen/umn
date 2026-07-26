b1ca9620e646b39577ddc30c9351959910e5a014
snorkungen <teuvo2004@gmail.com>
2026-07-26 07:38:04 +0300

    update: wasm.c

--- src/wasm.c
+++ src/wasm.c
@@ -1,34 +1,115 @@
-// process.c
 
 #define UMN_CORE_NOLIBC
 
-__attribute__((export_name("process_string"))) int process_string(const char *str, int length)
-{
-    // Example: Count the number of 'a' characters
-    int count = 0;
-    for (int i = 0; i < length; i++)
-    {
-        if (str[i] == 'a')
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-// Helper to reset memory if using a simple bump allocator
-__attribute__((export_name("reset_memory"))) void reset_memory();
-
 #define UMN_LEXER_IMPLEMENTATION
 #define UMN_CORE_IMPLEMENTATION
-
-#include "umn/lexer.h"
+#define UMN_EXPR_IMPLEMENTATION
 #include "umn/expr.h"
 
-__attribute__((export_name("tempalloc"))) void *tempalloc(size_t size)
+typedef enum
 {
-    static void *ptr = NULL;
-    return (ptr = umn_memrealloc(ptr, size));
+    Enc_Bin,
+    Enc_Oct,
+    Enc_Dec,
+    Enc_Hex,
+    Enc_Last
+} Enc;
+
+const char ENC_MAP_S[] = {
+    [Enc_Bin] = 'b',
+    [Enc_Oct] = 'o',
+    [Enc_Dec] = 'd',
+    [Enc_Hex] = 'x',
+};
+
+const char *ENC_MAP_L[] = {
+    [Enc_Bin] = "binary",
+    [Enc_Oct] = "octal",
+    [Enc_Dec] = "decimal",
+    [Enc_Hex] = "hex",
+};
+
+typedef struct
+{
+    umn_Lexer lexer;
+    bool output[Enc_Last];
+} Conf;
+
+Conf init_conf(int argc, char **argv)
+{
+    Conf config = {0};
+
+    umn_sb_t sb = {0};
+    umn_slice_reserve(sb, 128);
+    sb.items[0] = '\0';
+
+    for (int i = 1; i < argc; i++)
+        umn_sb_pushc(&sb, ' '), umn_sb_pushs(&sb, argv[i]);
+
+    umn_Token token;
+    static umn_Symbol symbols[] = {{"-"}, {"--"}}; /* What's the syntax */
+    umn_Lexer lexer = {
+        .data = sb.items,
+        .symbols = {
+            .count = ARRAY_LEN(symbols),
+            .items = symbols,
+        },
+    };
+
+    /* this magic fuckin loop does stuff etc .. k*/
+    while (umn_lexer_peek(&lexer, &token) == 0)
+    {
+        size_t token_end = token.begin + token.length;
+
+        if (umn_token_issymbol(&lexer, &token, "--"))
+        {
+            umn_lexer_take(&lexer, &token);
+            umn_lexer_peek(&lexer, &token);
+
+            if (token.begin != token_end)
+                break; /* this does not matter any more */
+
+            for (int i = 0; i < Enc_Last; i++)
+                config.output[i] = config.output[i] || umn_token_litcmp(&lexer, &token, ENC_MAP_L[i]) == 0;
+
+            umn_lexer_take(&lexer, &token);
+            continue;
+        }
+        else if (umn_token_issymbol(&lexer, &token, "-"))
+        {
+            umn_lexer_take(&lexer, &token);
+            umn_lexer_peek(&lexer, &token);
+
+            if (token.begin != token_end || token.kind != UMN_KLITERAL)
+                continue; /* let's move on this should error but oh well */
+
+            for (int j = 0; j < token.length; j++)
+            {
+                for (int i = 0; i < Enc_Last; i++)
+                    config.output[i] = config.output[i] || lexer.data[token.begin + j] == ENC_MAP_S[i];
+            }
+
+            umn_lexer_take(&lexer, &token);
+            continue;
+        }
+
+        break;
+    }
+
+    /* NOTE: leaking memory, this should just return the lexer */
+    config.lexer = lexer;
+
+    /* set default output */
+    {
+        unsigned int sum = 0;
+        for (int i = 0; i < Enc_Last; i++)
+            sum += config.output[i];
+
+        if (sum == 0)
+            config.output[Enc_Dec] = true;
+    }
+
+    return config;
 }
 
 static umn_Symbol umn_notation_symbols[] = {
@@ -45,6 +126,72 @@ static umn_Symbol umn_notation_symbols[] = {
     {"^", .flags = UMN_SF_BINOP, .data = 0x80},
     {"|", .flags = UMN_SF_BINOP, .data = 0x80},
 };
+
+/* TODO: support arbitrary precision numbers */
+typedef struct
+{
+    uint64_t value;
+} umn_uint_t;
+
+#define umn_uint_byte_count(__n__) ((((sizeof(__n__) * 8) - __builtin_clzl(__n__ | 1)) + 7) / 8)
+#define umn_uint(__n__) ((umn_uint_t){.value = __n__})
+
+umn_uint_t compute_node(const umn_Lexer *lexer, const umn_PToken *p, umn_Token *err_token)
+{
+    /* TODO: how do i's indicate an error ...*/
+    /* could just exfiltrate bya assigning onto som kind of err node */
+    /* this is ugly but I would pressume it to work */
+    if (err_token && err_token->kind)
+        return umn_uint(0);
+
+    umn_assert(p);
+
+    umn_uint_t value = {0}, lvalue, rvalue;
+    if (p->token.kind == UMN_KINTEGER)
+        return umn_uint(umn_token_readu(lexer, &p->token));
+    else if (p->token.kind & UMN_KBINOP && p->lvalue && p->rvalue)
+    {
+        lvalue = compute_node(lexer, p->lvalue, err_token);
+        rvalue = compute_node(lexer, p->rvalue, err_token);
+
+        if (NULL)
+            ;
+        else if (umn_token_issymbol(lexer, &p->token, "<<"))
+        {
+            lvalue.value <<= rvalue.value;
+        }
+        else if (umn_token_issymbol(lexer, &p->token, ">>"))
+        {
+            lvalue.value >>= rvalue.value;
+        }
+        else if (umn_token_issymbol(lexer, &p->token, "&"))
+        {
+            lvalue.value &= rvalue.value;
+        }
+        else if (umn_token_issymbol(lexer, &p->token, "^"))
+        {
+            lvalue.value ^= rvalue.value;
+        }
+        else if (umn_token_issymbol(lexer, &p->token, "|"))
+        {
+            lvalue.value |= rvalue.value;
+        }
+
+        return lvalue;
+    }
+    else if (p->token.kind & UMN_KUNARY_L && p->rvalue && umn_token_issymbol(lexer, &p->token, "~"))
+    {
+        value = compute_node(lexer, p->rvalue, err_token);
+        value.value = ~value.value ^ (~0UL << (umn_uint_byte_count(value.value) * 8));
+
+        return value;
+    }
+
+    if (err_token)
+        *err_token = p->token;
+
+    UMN_TODO("handle differing values");
+}
 
 __attribute__((export_name("count_tokens"))) int count_tokens(const char *str)
 {
@@ -63,4 +210,44 @@ __attribute__((export_name("count_tokens"))) int count_tokens(const char *str)
         count++;
 
     return count;
+}
+
+__attribute__((export_name("hex_encode"))) const char *hex_encode(const char *input)
+{
+    static char buffer[256];
+    umn_sb_t sb = {.capacity = sizeof(buffer), .items = buffer};
+    umn_PToken_Allocator ptokens = {0};
+    umn_Token token, err_token = {0};
+    const umn_PToken *p;
+    umn_Lexer lexer = {
+        .data = input,
+        .symbols = {
+            .count = ARRAY_LEN(umn_notation_symbols),
+            .items = umn_notation_symbols,
+        },
+    };
+
+    if ((p = umn_expr_parse(&lexer, &ptokens, NULL, NULL)))
+    {
+        if (p->token.kind & UMN_KERR)
+            return NULL;
+
+        umn_uint_t v = compute_node(&lexer, p, &err_token);
+
+        umn_sb_pushs(&sb, "0b");
+        umn_sb_pushu(&sb, v.value, 2);
+    }
+
+    return sb.items;
+}
+
+__attribute__((export_name("panic"))) void *panic()
+{
+    umn_panic("testing", __FILE__, __LINE__);
+}
+
+__attribute__((export_name("tempalloc"))) void *tempalloc(size_t size)
+{
+    static void *ptr = NULL;
+    return (ptr = umn_memrealloc(ptr, size));
 }
